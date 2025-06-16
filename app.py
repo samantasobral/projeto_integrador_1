f04f9addc650a41a48fd982e9f52765b4f662ea6
Samanta Sobral <samantasobral.ds@gmail.com>
2025-06-16 18:09:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 import pickle
 from empresa.empresa import PredictEmprestimo
 
-model = pickle.load(open('../model/final_model.pkl', 'rb'))
+model = pickle.load(open('model/final_model.pkl', 'rb'))
 
 app = Flask(__name__)
 
